1df4b4f109a7ad8beea63d121a0cc95fd7825972
TheGuyWhoPorts <121072594+TheGuyWhoPorts@users.noreply.github.com>
2022-12-20 21:51:43 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2249,8 +2249,8 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
-		+ ' | Rating: ' + ratingName
+		+ ' | Ported By Oswald Gamer| Misses: ' + songMisses
+		+ ' | Combo: ' + songCombo | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
 		if(ClientPrefs.scoreZoom && !miss && !cpuControlled)
